724daf8c18a008505241d7bf60c5547ac2b04307
Peter Shipley <peter.shipley@gmail.com>
2013-04-12 10:36:46 -0700

    Simple example program for listing the state of programs on an ISY device

---
+++ bin/isy_progs.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python2.7
+
+
+
+import ISY
+import pprint
+ 
+  
+myisy= ISY.Isy(addr="10.1.1.36", debug=2)
+   
+myisy.load_prog()
+
+
+pfmt = "{:<5}{:<24} {:<5} {:<5}{!s:<5}"
+print(pfmt.format("Id", "Node Name", "Stat", "Run", "Enabled"))
+print(pfmt.format("-" * 4, "-" * 10, "-" * 4, "-" * 4,"-" * 4))
+for p in myisy.prog_iter():
+   if p.folder :
+       print(pfmt.format(p.id, p.name, p.status, "-", "-"))
+   else:
+       print(pfmt.format(p.id, p.name, p.status, p.running, p.enabled))
+
+
+
