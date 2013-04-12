9292049ffd3ab8c64f07987535fa6c66da7e9874
Peter Shipley <peter.shipley@gmail.com>
2013-04-12 10:37:59 -0700

    a program for listing the status of Nodes on an  ISY (was isy_list.py )

---
+++ bin/isy_nodes.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python2.7
+
+import ISY
+
+
+def list_nodes(myisy) :
+    pfmt = "{:<20} {:<12}\t{:<12}{!s:<12}"
+    print(pfmt.format("Node Name", "Address", "Status", "enabled"))
+    print(pfmt.format("---------", "-------", "------", "------"))
+    for nod in myisy :
+       if nod.type == "scene" :
+           print(pfmt.format(nod.name, nod.address, "-", "-"))
+       else :
+           print(pfmt.format(nod.name, nod.address, nod.formatted, nod.enabled))
+       # print(" members : ", len(nod.members_list()))
+
+if __name__ == '__main__' :
+    myisy = ISY.Isy( )
+    list_nodes(myisy)
+
