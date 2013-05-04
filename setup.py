51a3368dd9ddafd7f3470c7df346a8511807b05e
Peter Shipley <peter.shipley@gmail.com>
2013-05-04 09:06:21 -0700

    move the example script to bin

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author='Peter Shipley',
     author_email='Peter.Shipley@gmail.com',
     packages=find_packages(),
-    data_files=[('examples', ['bin/isy_find.py','bin/isy_nodes.py',
+    data_files=[('bin', ['bin/isy_find.py','bin/isy_nodes.py',
     'bin/isy_progs.py', 'bin/isy_log.py',
     'bin/isy_var.py','bin/isy_showevents.py', 'bin/isy_nestset.py',
     'bin/isy_net_wol.py'])],
